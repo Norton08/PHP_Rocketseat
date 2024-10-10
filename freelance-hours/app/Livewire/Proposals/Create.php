b87d1db59be5222e2497bf5720817345f750b930
Norton Almeida <nortonalmeida08@gmail.com>
2024-10-10 01:04:05 -0300

    atualizando posicoes

--- freelance-hours/app/Livewire/Proposals/Create.php
+++ freelance-hours/app/Livewire/Proposals/Create.php
@@ -2,7 +2,10 @@
 
 namespace App\Livewire\Proposals;
 
+use App\Actions\ArrangePositions;
 use App\Models\Project;
+use App\Models\Proposal;
+use Illuminate\Support\Facades\DB;
 use Livewire\Attributes\Validate;
 use Livewire\Component;
 
@@ -19,6 +22,7 @@ class Create extends Component
 	public int $hours = 0;
 
 	public bool $agree = false;
+
     public function render()
     {
         return view('livewire.proposals.create');
@@ -33,14 +37,37 @@ class Create extends Component
 			return;
 		}
 
-		$this->dispatch('proposal::created');
-		 
-		$this->project->proposals()
+		DB::transaction(function() {
+			$proposal = $this->project->proposals()
 			->updateOrCreate(
 				['email' => $this->email],
 				['hours' => $this->hours]
 			);
-
+	
+			$this->arrangePositions($proposal);
+		});
+		
+		$this->dispatch('proposal::created');
 		$this->modal = false;
+	}
+
+	public function arrangePositions(Proposal $proposal)
+	{
+		$query = DB::select("
+			select 
+				*,
+				row_number() over (order by hours) as newPosition
+			from proposals
+			where project_id = {$proposal->project_id}
+		");
+		$position = collect($query)->where('id', "=", $proposal->id)->first();
+		$otherProposal = collect($query)->where('position', "=", $position->newPosition)->first();
+
+		if($otherProposal){
+			$proposal->update(['position_status' => 'up']);
+			Proposal::query()->where('id', '=', $otherProposal->id)->update(['position_status' => 'down']);
+		}
+
+		ArrangePositions::run($proposal->project_id);
 	}
 }
